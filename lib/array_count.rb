1b22876a3ca7fc4a790265e06bbfb899136fbb3a
Brook Beyene <brookbeyene37@gmail.com>
2020-05-02 04:21:38 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -13,4 +13,14 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  counter = 0 
+  index_array = 0
+  while index_array < array.count do 
+    if array[index_array].is_a? String 
+      if array[index_array].size > 0 
+        counter += 1
+    end
+    index_array += 1 
+  end
+  return counter
 end
